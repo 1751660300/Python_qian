cb6540af8b367deafead967870a689d9c80fab44
钱良虎 <1751660300@qq.com>
2020-05-20 20:21:35 +0800

    补充：yield关键字的使用

--- day27/04生成器.py
+++ day27/04生成器.py
@@ -1,6 +1,21 @@
 # -*- coding:utf-8 -*-
 # 使用()创建生成器
 
-num_list = (i for i in range(10))  # 生成器对象
-print(next(num_list))
+# num_list = (i for i in range(10))  # 生成器对象
+# print(next(num_list))
+
+
+def t_yield():
+    print("__________")
+    while True:
+        for i in range(10):
+            res = yield i
+            print("res:", res)
+
+
+if __name__ == '__main__':
+    t = t_yield()
+    print(next(t))
+    print(next(t))
+    print(t.send(7))
 
